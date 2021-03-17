da6ef1e011fed2e11129aa50abf3eb590355216d
hjwforever <hjwbelieve@foxmail.com>
2021-03-17 18:56:08 +0800

    initial commit

---
+++ src/main/java/edu/bjtu/example/ebookshop/EbookshopApplication.java
@@ -0,0 +1,13 @@
+package edu.bjtu.example.ebookshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbookshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbookshopApplication.class, args);
+	}
+
+}
